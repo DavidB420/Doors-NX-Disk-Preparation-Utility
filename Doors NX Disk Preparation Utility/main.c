75910323e3e556cf69338595abc5d28561212a54
David Badiei <37677391+DavidB420@users.noreply.github.com>
2022-08-10 14:32:27 -0400

    Add project files.

--- Doors NX Disk Preparation Utility/main.c
+++ Doors NX Disk Preparation Utility/main.c
@@ -1,17 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <windows.h>
+#include <winioctl.h>
+#include <stdbool.h>
 
 void zeroFillBuffer(char* ptr, int length);
 
-int main(int argc,char** argv)
+bool checkForDuplicates(DWORD* ptr,DWORD value);
+
+int main(int argc, char** argv)
 {
 	//Declare variables
 	FILE* osImage;
 	char userInputBuffer[_MAX_PATH];
 	errno_t errorCode;
 	DWORD val;
-	WCHAR driveBuffer[_MAX_PATH] = {0};
+	DWORD sizeOfReturn;
+	WCHAR driveBuffer[_MAX_PATH] = { 0 };
+	wchar_t logicalDriveAddr[_MAX_PATH] = {0};
+	DWORD driveNumbers[32] = {0xFFFFFFFF};
+	int driveNumberPointer = 0;
+	int inttemp;
 
 	//Initialize buffer by zero filling it
 	zeroFillBuffer(&userInputBuffer[0], _MAX_PATH);
@@ -28,16 +37,50 @@ int main(int argc,char** argv)
 
 	//Get and display available drives
 	val = GetLogicalDrives();
+	GetLogicalDriveStringsW(_MAX_PATH, &logicalDriveAddr);
 
 	printf("\nDisk #			Size (bytes)\n");
 	printf("------			-------------------------\n");
+
+	
+
+	for (int i = 0; i < 31; i++)
+	{
+		driveNumbers[i] = 0xFFFFFFFF;
+	}
+	
+	STORAGE_DEVICE_NUMBER fdisk = { 0 };
+	DISK_GEOMETRY diskSizeGeo = { 0 };
+	ULONGLONG diskSize = 0;
 
 	for (int i = 2; i < 31; i++)
 	{
 		if ((val & (1 << i)) >> i == 1)
 		{
-			printf("%i\n",i-2);
+			GetVolumeNameForVolumeMountPointW(&logicalDriveAddr[(i-2) * 4], &driveBuffer, _MAX_PATH);
+			driveBuffer[48] = 0;
+			HANDLE driveHandler = CreateFile(driveBuffer, 0, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+			DeviceIoControl(driveHandler, IOCTL_STORAGE_GET_DEVICE_NUMBER, NULL, 0, &fdisk, sizeof(fdisk), &sizeOfReturn,NULL);
+			if (!checkForDuplicates(&driveNumbers, fdisk.DeviceNumber))
+			{
+				DeviceIoControl(driveHandler, IOCTL_DISK_GET_DRIVE_GEOMETRY, NULL, 0, &diskSizeGeo, sizeof(diskSizeGeo), &sizeOfReturn, NULL);
+				diskSize = diskSizeGeo.Cylinders.QuadPart * (ULONG)diskSizeGeo.TracksPerCylinder * (ULONG)diskSizeGeo.SectorsPerTrack * (ULONG)diskSizeGeo.BytesPerSector;
+				printf("%i			%I64d\n", fdisk.DeviceNumber,diskSize);
+				driveNumbers[driveNumberPointer] = fdisk.DeviceNumber;
+				driveNumberPointer++;
+			}
+			CloseHandle(driveHandler);
 		}
+	}
+
+	zeroFillBuffer(&userInputBuffer[0], _MAX_PATH);
+	printf("\nPlease select a drive with its drive number: ");
+	scanf_s("%[^\n]%*c", userInputBuffer, _MAX_PATH);
+
+	while (!checkForDuplicates(&driveNumbers, atoi(&userInputBuffer[0])))
+	{
+		printf("ERROR: Invalid disk choice!\nPlease try again: ");
+		scanf_s("%[^\n]%*c", userInputBuffer, _MAX_PATH);
 	}
 
 	return 0;
@@ -49,4 +92,16 @@ void zeroFillBuffer(char *ptr, int length)
 	{
 		ptr[i] = 0;
 	}
+}
+
+bool checkForDuplicates(DWORD* ptr, DWORD value)
+{
+	for (int i = 0; i < 31; i++)
+	{
+		if (ptr[i] == value)
+		{
+			return true;
+		}
+	}
+	return false;
 }
